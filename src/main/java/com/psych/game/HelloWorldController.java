2c7fdf8ead20cb001707440270ce59f8cddb0d0b
Ramesh <rameshsapphire1996@gmail.com>
2020-05-12 17:27:06 +0530

    init

---
+++ src/main/java/com/psych/game/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.psych.game;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dev-test")
+public class HelloWorldController {
+    @GetMapping("/")
+    String hello(){
+        return "Hello World";
+    }
+
+}
